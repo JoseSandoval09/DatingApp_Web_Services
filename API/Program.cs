2d0fdf7e9fa2009b305072b2073cd2a41c187865
José Sandoval <joseeesp.09@gmail.com>
2025-09-07 20:09:21 -0600

    Display fetched users

--- API/Program.cs
+++ API/Program.cs
@@ -14,9 +14,16 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200",
+                 "https://localhost:4200"
+    ));
 
 
-app.UseHttpsRedirection();
+
+
+
 
 app.UseAuthorization();
 
