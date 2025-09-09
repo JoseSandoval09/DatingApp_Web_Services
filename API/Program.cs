f44fb29d13a52f1fd10499a76296cbc05aafcfba
José Sandoval <114935040+JoseSandoval09@users.noreply.github.com>
2025-09-09 07:38:32 -0600

    Parcial 01 - Add API and Client Skeleton (#1)
    * Partial 01 - Initial Commit
    
    * Modify files
    
    * Add first entity
    
    * Add entityframework
    
    * Add fisrtMigration
    
    * Create database
    
    * Add new controller
    
    * Add async code in Members controllers
    
    * Add Angular skeleton
    
    * Add http client to SPA
    
    * Display fetched users
    
    * Add Tailwind and DaisyUI for styling
    
    * Style fetched info
    
    * Add ssl

---
+++ API/Program.cs
@@ -0,0 +1,32 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
+});
+
+
+var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200",
+                 "https://localhost:4200"
+    ));
+
+
+
+
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
