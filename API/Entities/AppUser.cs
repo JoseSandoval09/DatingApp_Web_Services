0f80a986471fcb62b867173bac407246f3b9e56c
José Sandoval <joseeesp.09@gmail.com>
2025-10-19 22:50:02 -0600

    Add Members migrations and seed user

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -10,8 +10,14 @@ public class AppUser
 
     public required string Email { get; set; }
 
+    public string? ImageUrl { get; set; }
+
     public required byte[] PasswordHash { get; set; }
 
     public required byte[] PasswordSalt { get; set; }
+
+    // Navigation property
+
+    public Member Member { get; set; } = null!;
 
 }
