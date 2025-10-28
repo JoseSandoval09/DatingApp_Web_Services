0e49c7dfae3aa946bfa31b2365509e304cff965f
José Sandoval <joseeesp.09@gmail.com>
2025-10-28 07:37:09 -0600

    Add member-details page

--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../../core/services/members-service';
+import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Member } from '../../../types/member';
 
 @Component({
   selector: 'app-member-detail',
-  imports: [],
+  imports: [AsyncPipe, RouterLink, RouterLinkActive],
   templateUrl: './member-detail.html',
   styleUrl: './member-detail.css'
 })
-export class MemberDetail {
+export class MemberDetail implements OnInit {
+  private memberService = inject(MembersService);
+  private route = inject(ActivatedRoute);
+  protected member$?: Observable<Member>;
 
+  ngOnInit(): void {
+    this.member$ = this.loadMember();
+  }
+
+  loadMember(): Observable<Member> | undefined {
+    const id = this.route.snapshot.paramMap.get('id');
+    if(id){
+      return this.memberService.getMember(id);
+
+    }
+
+    return;
+  }
 }
