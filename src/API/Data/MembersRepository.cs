4694bc9df551caa3396c95131d27d6870a92245b
José Sandoval <joseeesp.09@gmail.com>
2026-03-19 11:39:23 -0600

    Add filtering to API v3

--- src/API/Data/MembersRepository.cs
+++ src/API/Data/MembersRepository.cs
@@ -29,6 +29,10 @@ public class MembersRepository(AppDbContext context) : IMembersRepository
             query = query.Where(x => x.Gender == request.Gender);
         }
 
+        var minAgeDate = DateOnly.FromDateTime(DateTime.Today.AddYears(-request.MaxAge - 1));
+        var maxAgeDate = DateOnly.FromDateTime(DateTime.Today.AddYears(-request.MinAge));
+        query = query.Where(x => x.BirthDate >= minAgeDate && x.BirthDate <= maxAgeDate);
+
         return await Pagination.CreateAsync(query, request.PageNumber, request.PageSize);
     }
     public async Task<IReadOnlyList<Photo>> GetPhotosAsync(string memberId)
