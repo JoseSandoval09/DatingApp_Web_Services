e5898be8dc5f58334b50554a670b58c3c14131b1
José Sandoval <joseeesp.09@gmail.com>
2026-03-26 10:35:29 -0600

    Pacial05 - Add age gender filters to modal

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -50,7 +50,8 @@ export class MemberList implements OnInit {
   }
 
   onFilterChange(data: MemberParams) {
-    console.log('Modal submitted data: ', data);
+    this.memberParams = data;
+    this.loadMembers();
   }
 
   resetFilters() {
