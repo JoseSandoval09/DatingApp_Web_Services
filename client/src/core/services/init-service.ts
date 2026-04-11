efa434258780e4c971ef81a2c333d311fa1dea85
José Sandoval <joseeesp.09@gmail.com>
2026-04-11 15:58:14 -0600

    Parcial 05 - Add Likes service

--- client/src/core/services/init-service.ts
+++ client/src/core/services/init-service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { AccountService } from './account-service';
 import { Observable, of } from 'rxjs';
+import { LikesService } from './likes-service';
 
 
 @Injectable({
@@ -8,12 +9,15 @@ import { Observable, of } from 'rxjs';
 })
 export class InitService {
   private accountService = inject(AccountService);
+  private likesService = inject(LikesService);
+
 
   init(): Observable<null> {
     const userString = localStorage.getItem('user');
     if (userString) {
       const user = JSON.parse(userString);
       this.accountService.setCurrentUser(user);
+      this.likesService.getLikeIds();
     }
     return of(null);
   }
