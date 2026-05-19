54089415ae053f830e39cba041029e71fc3af7e8
José Sandoval <joseeesp.09@gmail.com>
2026-05-19 16:39:42 -0600

    Parcial06 - IDENTITY - Add has-role directive

---
+++ client/src/shared/directives/has-role.spec.ts
@@ -0,0 +1,9 @@
+
+import { HasRole } from './has-role';
+
+describe('HasRole', () => {
+  it('should create an instance', () => {
+    const directive = new HasRole();
+    expect(directive).toBeTruthy();
+  });
+});
