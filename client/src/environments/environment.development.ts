178efb322925982fe2c466fdc91748a69cc6cab0
José Sandoval <joseeesp.09@gmail.com>
2026-05-19 17:06:20 -0600

    Parcial06 - SignalR - Add presence hub to client

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: "https://localhost:7131/api/"
+  apiUrl: "api/",
+  hubUrl: "hubs/"
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "api/"
+  apiUrl: "https://localhost:7131/api/",
+  hubUrl: "https://localhost:7131/hubs/",
 };
