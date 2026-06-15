12401b2b313de61c4e079eeeec9a3808f53b3a77
José Sandoval <joseeesp.09@gmail.com>
2026-06-15 11:51:27 -0600

    Parcial06 - SignalR - Fix to GetUserID

--- src/API/SignalR/MessageHub.cs
+++ src/API/SignalR/MessageHub.cs
@@ -1,7 +1,6 @@
-using System.Security.Claims;
-
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using API.Mappers;
 using Microsoft.AspNetCore.Authorization;
@@ -55,7 +54,6 @@ public class MessageHub(IMessagesRepository messagesRepository,
         if (await messagesRepository.SaveAllAsync())
         {
             await Clients.Group(groupName).SendAsync("NewMessage", message.ToResponse());
-
         }
     }
 
@@ -89,6 +87,6 @@ public class MessageHub(IMessagesRepository messagesRepository,
 
     private string GetUserId()
     {
-        return Context.User?.FindFirstValue(ClaimTypes.Email) ?? throw new HubException("Cannot get member id");
+        return Context.User?.GetMemberId() ?? throw new HubException("Cannot get member id");
     }
 }
--- src/API/SignalR/PresenceHub.cs
+++ src/API/SignalR/PresenceHub.cs
@@ -1,6 +1,7 @@
-using System.Security.Claims;
+using API.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+
 namespace API.SignalR;
 
 [Authorize]
@@ -23,7 +24,7 @@ public class PresenceHub(PresenceTracker presenceTracker) : Hub
 
     private string GetUserId()
     {
-        return Context.User?.FindFirstValue(ClaimTypes.Email) ?? throw new HubException("Cannot get member id");
+        return Context.User?.GetMemberId() ?? throw new HubException("Cannot get member id");
     }
 
     private async Task GetOnlineUsers()
