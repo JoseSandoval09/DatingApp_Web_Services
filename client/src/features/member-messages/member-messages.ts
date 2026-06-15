dac39c6af2c6833782ac3e2c3c2bd3e6d5e9ba47
José Sandoval <joseeesp.09@gmail.com>
2026-06-15 11:31:15 -0600

    Parcial06 - SignalR - Update message component in client

--- client/src/features/member-messages/member-messages.ts
+++ client/src/features/member-messages/member-messages.ts
@@ -6,6 +6,7 @@ import { DatePipe } from '@angular/common';
 import { TimeAgoPipe } from '../../core/pipes/time-ago-pipe';
 import { FormsModule } from '@angular/forms';
 import { PresenceService } from '../../core/services/presence-service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-member-messages',
@@ -15,11 +16,12 @@ import { PresenceService } from '../../core/services/presence-service';
 })
 export class MemberMessages implements OnInit {
   @ViewChild('messageEndRef') messageEndRef!: ElementRef;
-  private messagesService = inject(MessagesService);
+  protected messagesService = inject(MessagesService);
   private membersService = inject(MembersService);
   protected messages = signal<Message[]>([]);
   protected messageContent = '';
   protected presenceService = inject(PresenceService);
+  private router = inject(ActivatedRoute);
 
   constructor() {
     effect(() => {
@@ -31,7 +33,13 @@ export class MemberMessages implements OnInit {
   }
 
   ngOnInit(): void {
-    this.loadMessages();
+    this.router.parent?.paramMap.subscribe({
+      next: params => {
+        const otherUserId = params.get('id');
+        if (!otherUserId) throw new Error('Cannot connect to hub');
+        this.messagesService.createHubConnection(otherUserId);
+      }
+    })
   }
 
   loadMessages() {
